23d347a717a4d92781ffd4d7e2ad5f44475ffe60
HabitPoot <137962722+HabitPoot@users.noreply.github.com>
2023-06-28 15:28:32 -0700

    up

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+let template = {
+    "esv": [],
+    "krv": [],
+    "date": "2023-06-27T00:00:00",
+    "book": "Genesis",
+    "kbook": "창세기",
+    "chapter": 1,
+    "start": 1,
+    "end": 5
+}
 let data = [
     {
-        "day": 1,
         "esv": [
             "1| In the beginning, God created the heavens and the earth.",
             "2| The earth was without form and void, and darkness was over the face of the deep. And the Spirit of God was hovering over the face of the waters",
@@ -15,25 +24,34 @@ let data = [
             "4| 그 빛이 하나님의 보시기에 좋았더라 하나님이 빛과 어두움을 나누사",
             "5| 빛을 낮이라 칭하시고 어두움을 밤이라 칭하시니라 저녁이 되며 아침이 되니 이는 첫째 날이니라"
         ],
-        "date": "2023-06-27T00:00:00"
+        "date": "2023-06-27T00:00:00",
+        "book": "Genesis",
+        "kbook": "창세기",
+        "chapter": 1,
+        "start": 1,
+        "end": 5
     },
     {
-        "day":2,
-        "esv":[
+        "esv": [
             "6| And God said, \"Let there be an expanse in the midst of the waters, and let it separate the waters from the waters.\"",
             "7| And God made the expanse and separated the waters that were under the expanse from the waters that were above the expanse. And it was so.",
             "8| And God called the expanse Heaven. And there was evening and there was morning, the second day.",
             "9| And God said, \"Let the waters under the heavens be gathered together into one place, and let the dry land appear.\" And it was so.",
             "10| God called the dry land Earth, and the waters that were gathered together he called Seas. And God saw that it was good."
         ],
-        "krv":[
+        "krv": [
             "6| 하나님이 가라사대 물 가운데 궁창이 있어 물과 물로 나뉘게 하리라 하시고",
             "7| 하나님이 궁창을 만드사 궁창 아래의 물과 궁창 위의 물로 나뉘게 하시매 그대로 되니라",
             "8| 하나님이 궁창을 하늘이라 칭하시니라 저녁이 되며 아침이 되니 이는 둘째 날이니라",
             "9| 하나님이 가라사대 천하의 물이 한곳으로 모이고 뭍이 드러나라 하시매 그대로 되니라",
             "10| 하나님이 뭍을 땅이라 칭하시고 모인 물을 바다라 칭하시니라 하나님의 보시기에 좋았더라"
         ],
-        "date":"2023-06-28T00:00:00"
+        "date": "2023-06-28T00:00:00",
+        "book": "Genesis",
+        "kbook": "창세기",
+        "chapter": 1,
+        "start": 6,
+        "end": 10
     }
 ]
 let root = document.getElementById("root")
@@ -62,13 +80,13 @@ function retrieveSettings() {
 }
 
 function updateDay(direction) {
-    let pos = data.findIndex(d=>d===current)
-    switch(direction){
+    let pos = data.findIndex(d => d === current)
+    switch (direction) {
         case 'left':
-            if(pos > 0) pos--
+            if (pos > 0) pos--
             break
         case 'right':
-            if(pos < data.length - 1) pos++
+            if (pos < data.length - 1) pos++
             break
     }
     current = data[pos]
@@ -108,13 +126,16 @@ function setLanguage(lang) {
     root.innerHTML = ""
     switch (lang) {
         case 'both':
-            for(let i = 0; i < current.krv.length; i++) {
+            root.innerHTML += `<h3 class="text-tags">${current.kbook} ${current.chapter}:${current.start}-${current.end}</h3>`
+            root.innerHTML += `<h3 class="text-tags">${current.book} ${current.chapter}:${current.start}-${current.end}</h3>`
+            for (let i = 0; i < current.krv.length; i++) {
                 root.innerHTML += `<p class="text-tags">${current.krv[i]}</p>`
                 root.innerHTML += `<p class="text-tags">${current.esv[i]}</p>`
             }
             break
         default:
-            for(let verse of current[lang]) {
+            root.innerHTML += `<h3 class="text-tags">${lang === 'krv' ? current.kbook : current.book} ${current.chapter}:${current.start}-${current.end}</h3>`
+            for (let verse of current[lang]) {
                 root.innerHTML += `<p class="text-tags">${verse}</p>`
             }
     }
@@ -128,19 +149,19 @@ dark.addEventListener("click", function () {
     colorMode('dark')
 })
 
-esv.addEventListener("click", function(){
+esv.addEventListener("click", function () {
     setLanguage('esv')
 })
-krv.addEventListener("click", function(){
+krv.addEventListener("click", function () {
     setLanguage('krv')
 })
-both.addEventListener("click", function(){
+both.addEventListener("click", function () {
     setLanguage('both')
 })
-left.addEventListener("click", function(){
+left.addEventListener("click", function () {
     updateDay('left')
 })
-right.addEventListener("click", function(){
+right.addEventListener("click", function () {
     updateDay('right')
 })
 
