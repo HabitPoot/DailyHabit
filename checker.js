1f7078cf6c35573a5baf2fab4a0e32fc7ea4c1b4
HabitPoot <137962722+HabitPoot@users.noreply.github.com>
2023-07-06 16:55:11 -0700

    day 10

--- checker.js
+++ checker.js
@@ -1,44 +1,51 @@
 mine = [
-    "1| Now Adam knew Eve his wife, and she conceived and bore Cain, saying, \"I have gotten a man with the help of the Lord.\"",
-    "2| And again, she bore his brother Abel. Now Abel was a keeper of sheep, and Cain a worker of the ground.",
-    "3| In the course of time Cain brought to the Lord an offering of the fruit of the ground,",
-    "4| and Abel also brought of the firstborn of his flock and of their fat portions. And the Lord had regard for Abel and his offering,",
-    "5| but for Cain and his offering he had no regard. So Cain was very angry, and his face fell.",
-    "6| The Lord said to Cain, \"Why are you angry, and why has your face fallen?",
-    "7| If you do well, will you not be accepted? And if you do not do well, sin is crouching at the door. Its desire is contrary to you, but you must rule over it.\"",
-    "8| Cain spoke to Abel his brother. And when they were in the field, Cain rose up against his brother Abel and killed him.",
-    "9| Then the Lord said to Cain, \"Where is Abel your brother?\" He said, \"I do not know; am I my brother's keeper?\"",
-    "10| And the Lord said, \"What have you done? The voice of your brother's blood is crying to me from the ground.",
-    "11| And now you are cursed from the ground, which has opened its mouth to receive your brother's blood from your hand.",
-    "12| When you work the ground, it shall no longer yield to you its strength. You shall be a fugitive and a wanderer on the earth.\"",
-    "13| Cain said to the Lord, \"My punishment is greater than I can bear.",
-    "14| Behold, you have driven me today away from the ground, and from your face I shall be hidden. I shall be a fugitive and a wanderer on the earth, and whoever finds me will kill me.\"",
-    "15| Then the Lord said to him, \"Not so! If anyone kills Cain, vengeance shall be taken on him sevenfold.\" And the Lord put a mark on Cain, lest any who found him should attack him.",
-    "16| Then Cain went away from the presence of the Lord and settled in the land of Nod, east of Eden.",
-    "17| Cain knew his wife, and she conceived and bore Enoch. When he built a city, he called the name of the city after the name of his son, Enoch.",
-    "18| To Enoch was born Irad, and Irad fathered Mehujael, and Mehujael fathered Methushael, and Methushael fathered Lamech.",
-    "19| And Lamech took two wives. The name of the one was Adah, and the name of the other Zillah.",
-    "20| Adah bore Jabal; he was the father of those who dwell in tents and have livestock.",
-    "21| His brother's name was Jubal; he was the father of all those who play the lyre and pipe.",
-    "22| Zillah also bore Tubal-cain; he was the forger of all instruments of bronze and iron. The sister of Tubal-cain was Naamah.",
-    "23| Lamech said to his wives: \"Adah and Zillah, hear my voice; you wives of Lamech, listen to what I say: I have killed a man for wounding me, a young man for striking me.",
-    "24| If Cain's revenge is sevenfold, then Lamech's is seventy-sevenfold.\"",
-    "25| And Adam knew his wife again, and she bore a son and called his name Seth, for she said, \"God has appointed for me another offspring instead of Abel, for Cain killed him.\"",
-    "26| To Seth also a son was born, and he called his name Enosh. At that time people began to call upon the name of the Lord."
+    "1| This is the book of the generations of Adam. When God created man, he made him in the likeness of God.",
+    "2| Male and female he created them, and he blessed them and named them Man when they were created.",
+    "3| When Adam had lived 130 years, he fathered a son in his own likeness, after his image, and named him Seth.",
+    "4| The days of Adam after he fathered Seth were 800 years; and he had other sons and daughters.",
+    "5| Thus all the days that Adam lived were 930 years, and he died.",
+    "6| When Seth had lived 105 years, he fathered Enosh.",
+    "7| Seth lived after he fathered Enosh 807 years and had other sons and daughters.",
+    "8| Thus all the days of Seth were 912 years, and he died.",
+    "9| When Enosh had lived 90 years, he fathered Kenan.",
+    "10| Enosh lived after he fathered Kenan 815 years and had other sons and daughters.",
+    "11| Thus all the days of Enosh were 905 years, and he died.",
+    "12| When Kenan had lived 70 years, he fathered Mahalalel.",
+    "13| Kenan lived after he fathered Mahalalel 840 years and had other sons and daughters.",
+    "14| Thus all the days of Kenan were 910 years, and he died.",
+    "15| When Mahalalel had lived 65 years, he fathered Jared.",
+    "16| Mahalalel lived after he fathered Jared 830 years and had other sons and daughters.",
+    "17| Thus all the days of Mahalalel were 895 years, and he died.",
+    "18| When Jared had lived 162 years, he fathered Enoch.",
+    "19| Jared lived after he fathered Enoch 800 years and had other sons and daughters.",
+    "20| Thus all the days of Jared were 962 years, and he died.",
+    "21| When Enoch had lived 65 years, he fathered Methuselah.",
+    "22| Enoch walked with God after he fathered Methuselah 300 years and had other sons and daughters.",
+    "23| Thus all the days of Enoch were 365 years.",
+    "24| Enoch walked with God, and he was not, for God took him.",
+    "25| When Methuselah had lived 187 years, he fathered Lamech.",
+    "26| Methuselah lived after he fathered Lamech 782 years and had other sons and daughters.",
+    "27| Thus all the days of Methuselah were 969 years, and he died.",
+    "28| When Lamech had lived 182 years, he fathered a son",
+    "29| and called his name Noah, saying, \"Out of the ground that the Lord has cursed, this one shall bring us relief from our work and from the painful toil of our hands.\"",
+    "30| Lamech lived after he fathered Noah 595 years and had other sons and daughters.",
+    "31| Thus all the days of Lamech were 777 years, and he died.",
+    "32| After Noah was 500 years old, Noah fathered Shem, Ham, and Japheth."
 ]
-
-theirs = `1Now Adam knew Eve his wife, and she conceived and bore Cain, saying, "I have gotten a man with the help of the Lord." 2And again, she bore his brother Abel. Now Abel was a keeper of sheep, and Cain a worker of the ground. 3In the course of time Cain brought to the Lord an offering of the fruit of the ground, 4and Abel also brought of the firstborn of his flock and of their fat portions. And the Lord had regard for Abel and his offering, 5but for Cain and his offering he had no regard. So Cain was very angry, and his face fell. 6The Lord said to Cain, "Why are you angry, and why has your face fallen? 7If you do well, will you not be accepted? And if you do not do well, sin is crouching at the door. Its desire is contrary to you, but you must rule over it."
-8Cain spoke to Abel his brother. And when they were in the field, Cain rose up against his brother Abel and killed him. 9Then the Lord said to Cain, "Where is Abel your brother?" He said, "I do not know; am I my brother's keeper?" 10And the Lord said, "What have you done? The voice of your brother's blood is crying to me from the ground. 11And now you are cursed from the ground, which has opened its mouth to receive your brother's blood from your hand. 12When you work the ground, it shall no longer yield to you its strength. You shall be a fugitive and a wanderer on the earth." 13Cain said to the Lord, "My punishment is greater than I can bear. 14Behold, you have driven me today away from the ground, and from your face I shall be hidden. I shall be a fugitive and a wanderer on the earth, and whoever finds me will kill me." 15Then the Lord said to him, "Not so! If anyone kills Cain, vengeance shall be taken on him sevenfold." And the Lord put a mark on Cain, lest any who found him should attack him. 16Then Cain went away from the presence of the Lord and settled in the land of Nod, east of Eden.
-17Cain knew his wife, and she conceived and bore Enoch. When he built a city, he called the name of the city after the name of his son, Enoch. 18To Enoch was born Irad, and Irad fathered Mehujael, and Mehujael fathered Methushael, and Methushael fathered Lamech. 19And Lamech took two wives. The name of the one was Adah, and the name of the other Zillah. 20Adah bore Jabal; he was the father of those who dwell in tents and have livestock. 21His brother's name was Jubal; he was the father of all those who play the lyre and pipe. 22Zillah also bore Tubal-cain; he was the forger of all instruments of bronze and iron. The sister of Tubal-cain was Naamah.
-23Lamech said to his wives:
-"Adah and Zillah, hear my voice;
-you wives of Lamech, listen to what I say:
-I have killed a man for wounding me,
-a young man for striking me.
-24If Cain's revenge is sevenfold,
-then Lamech's is seventy-sevenfold."
-25And Adam knew his wife again, and she bore a son and called his name Seth, for she said, "God has appointed for me another offspring instead of Abel, for Cain killed him." 26To Seth also a son was born, and he called his name Enosh. At that time people began to call upon the name of the Lord.`
+let encoded = ` “ ” `
+theirs = `1This is the book of the generations of Adam. When God created man, he made him in the likeness of God. 2Male and female he created them, and he blessed them and named them Man when they were created. 3When Adam had lived 130 years, he fathered a son in his own likeness, after his image, and named him Seth. 4The days of Adam after he fathered Seth were 800 years; and he had other sons and daughters. 5Thus all the days that Adam lived were 930 years, and he died.
+6When Seth had lived 105 years, he fathered Enosh. 7Seth lived after he fathered Enosh 807 years and had other sons and daughters. 8Thus all the days of Seth were 912 years, and he died.
+9When Enosh had lived 90 years, he fathered Kenan. 10Enosh lived after he fathered Kenan 815 years and had other sons and daughters. 11Thus all the days of Enosh were 905 years, and he died.
+12When Kenan had lived 70 years, he fathered Mahalalel. 13Kenan lived after he fathered Mahalalel 840 years and had other sons and daughters. 14Thus all the days of Kenan were 910 years, and he died.
+15When Mahalalel had lived 65 years, he fathered Jared. 16Mahalalel lived after he fathered Jared 830 years and had other sons and daughters. 17Thus all the days of Mahalalel were 895 years, and he died.
+18When Jared had lived 162 years, he fathered Enoch. 19Jared lived after he fathered Enoch 800 years and had other sons and daughters. 20Thus all the days of Jared were 962 years, and he died.
+21When Enoch had lived 65 years, he fathered Methuselah. 22Enoch walked with God after he fathered Methuselah 300 years and had other sons and daughters. 23Thus all the days of Enoch were 365 years. 24Enoch walked with God, and he was not, for God took him.
+25When Methuselah had lived 187 years, he fathered Lamech. 26Methuselah lived after he fathered Lamech 782 years and had other sons and daughters. 27Thus all the days of Methuselah were 969 years, and he died.
+28When Lamech had lived 182 years, he fathered a son 29and called his name Noah, saying, "Out of the ground that the Lord has cursed, this one shall bring us relief from our work and from the painful toil of our hands." 30Lamech lived after he fathered Noah 595 years and had other sons and daughters. 31Thus all the days of Lamech were 777 years, and he died.
+32After Noah was 500 years old, Noah fathered Shem, Ham, and Japheth.`
 theirs = theirs.split("\n").join(" ")
+theirs = theirs.split("“").join("")
+theirs = theirs.split("”").join("")
 console.log(mine.map(v => v.split("| ").join("")).join(" ") === theirs)
 
 let sample = ""
