e76da2d2d84750125873d5bce8092b95ac9e9325
HabitPoot <137962722+HabitPoot@users.noreply.github.com>
2023-07-10 19:09:06 -0700

    updated again

--- checker.js
+++ checker.js
@@ -1,32 +1,52 @@
 mine = [
-    "1| But God remembered Noah and all the beasts and all the livestock that were with him in the ark. And God made a wind blow over the earth, and the waters subsided.",
-    "2| The fountains of the deep and the windows of the heavens were closed, the rain from the heavens was restrained,",
-    "3| and the waters receded from the earth continually. At the end of 150 days the waters had abated,",
-    "4| and in the seventh month, on the seventeenth day of the month, the ark came to rest on the mountains of Ararat.",
-    "5| And the waters continued to abate until the tenth month; in the tenth month, on the first day of the month, the tops of the mountains were seen.",
-    "6| At the end of forty days Noah opened the window of the ark that he had made",
-    "7| and sent forth a raven. It went to and fro until the waters were dried up from the earth.",
-    "8| Then he sent forth a dove from him, to see if the waters had subsided from the face of the ground.",
-    "9| But the dove found no place to set her foot, and she returned to him to the ark, for the waters were still on the face of the whole earth. So he put out his hand and took her and brought her into the ark with him.",
-    "10| He waited another seven days, and again he sent forth the dove out of the ark.",
-    "11| And the dove came back to him in the evening, and behold, in her mouth was a freshly plucked olive leaf. So Noah knew that the waters had subsided from the earth.",
-    "12| Then he waited another seven days and sent forth the dove, and she did not return to him anymore.",
-    "13| In the six hundred and first year, in the first month, the first day of the month, the waters were dried from off the earth. And Noah removed the covering of the ark and looked, and behold, the face of the ground was dry.",
-    "14| In the second month, on the twenty-seventh day of the month, the earth had dried out.",
-    "15| Then God said to Noah,",
-    "16| \"Go out from the ark, you and your wife, and your sons and your sons' wives with you.",
-    "17| Bring out with you every living thing that is with you of all flesh-birds and animals and every creeping thing that creeps on the earth-that they may swarm on the earth, and be fruitful and multiply on the earth.\"",
-    "18| So Noah went out, and his sons and his wife and his sons' wives with him.",
-    "19| Every beast, every creeping thing, and every bird, everything that moves on the earth, went out by families from the ark.",
-    "20| Then Noah built an altar to the Lord and took some of every clean animal and some of every clean bird and offered burnt offerings on the altar.",
-    "21| And when the Lord smelled the pleasing aroma, the Lord said in his heart, \"I will never again curse the ground because of man, for the intention of man's heart is evil from his youth. Neither will I ever again strike down every living creature as I have done.",
-    "22| While the earth remains, seedtime and harvest, cold and heat, summer and winter, day and night, shall not cease.\""
+    "1| And God blessed Noah and his sons and said to them, \"Be fruitful and multiply and fill the earth.",
+    "2| The fear of you and the dread of you shall be upon every beast of the earth and upon every bird of the heavens, upon everything that creeps on the ground and all the fish of the sea. Into your hand they are delivered.",
+    "3| Every moving thing that lives shall be food for you. And as I gave you the green plants, I give you everything.",
+    "4| But you shall not eat flesh with its life, that is, its blood.",
+    "5| And for your lifeblood I will require a reckoning: from every beast I will require it and from man. From his fellow man I will require a reckoning for the life of man.",
+    "6|  \"Whoever sheds the blood of man, by man shall his blood be shed, for God made man in his own image.",
+    "7| And you, be fruitful and multiply, increase greatly on the earth and multiply in it.\"",
+    "8| Then God said to Noah and to his sons with him,",
+    "9| \"Behold, I establish my covenant with you and your offspring after you,",
+    "10| and with every living creature that is with you, the birds, the livestock, and every beast of the earth with you, as many as came out of the ark; it is for every beast of the earth.",
+    "11| I establish my covenant with you, that never again shall all flesh be cut off by the waters of the flood, and never again shall there be a flood to destroy the earth.\"",
+    "12| And God said, \"This is the sign of the covenant that I make between me and you and every living creature that is with you, for all future generations:",
+    "13| I have set my bow in the cloud, and it shall be a sign of the covenant between me and the earth.",
+    "14| When I bring clouds over the earth and the bow is seen in the clouds,",
+    "15| I will remember my covenant that is between me and you and every living creature of all flesh. And the waters shall never again become a flood to destroy all flesh.",
+    "16| When the bow is in the clouds, I will see it and remember the everlasting covenant between God and every living creature of all flesh that is on the earth.\"",
+    "17| God said to Noah, \"This is the sign of the covenant that I have established between me and all flesh that is on the earth.\"",
+    "18| The sons of Noah who went forth from the ark were Shem, Ham, and Japheth. (Ham was the father of Canaan.)",
+    "19| These three were the sons of Noah, and from these the people of the whole earth were dispersed.",
+    "20| Noah began to be a man of the soil, and he planted a vineyard.",
+    "21| He drank of the wine and became drunk and lay uncovered in his tent.",
+    "22| And Ham, the father of Canaan, saw the nakedness of his father and told his two brothers outside.",
+    "23| Then Shem and Japheth took a garment, laid it on both their shoulders, and walked backward and covered the nakedness of their father. Their faces were turned backward, and they did not see their father's nakedness.",
+    "24| When Noah awoke from his wine and knew what his youngest son had done to him,",
+    "25| he said, \"Cursed be Canaan; a servant of servants shall he be to his brothers.\"",
+    "26| He also said, \"Blessed be the Lord, the God of Shem; and let Canaan be his servant.",
+    "27| May God enlarge Japheth, and let him dwell in the tents of Shem, and let Canaan be his servant.\"",
+    "28| After the flood Noah lived 350 years.",
+    "29| All the days of Noah were 950 years, and he died."
 ]
 let encoded = ` “ ” `
-theirs = `1But God remembered Noah and all the beasts and all the livestock that were with him in the ark. And God made a wind blow over the earth, and the waters subsided. 2The fountains of the deep and the windows of the heavens were closed, the rain from the heavens was restrained, 3and the waters receded from the earth continually. At the end of 150 days the waters had abated, 4and in the seventh month, on the seventeenth day of the month, the ark came to rest on the mountains of Ararat. 5And the waters continued to abate until the tenth month; in the tenth month, on the first day of the month, the tops of the mountains were seen.
-6At the end of forty days Noah opened the window of the ark that he had made 7and sent forth a raven. It went to and fro until the waters were dried up from the earth. 8Then he sent forth a dove from him, to see if the waters had subsided from the face of the ground. 9But the dove found no place to set her foot, and she returned to him to the ark, for the waters were still on the face of the whole earth. So he put out his hand and took her and brought her into the ark with him. 10He waited another seven days, and again he sent forth the dove out of the ark. 11And the dove came back to him in the evening, and behold, in her mouth was a freshly plucked olive leaf. So Noah knew that the waters had subsided from the earth. 12Then he waited another seven days and sent forth the dove, and she did not return to him anymore.
-13In the six hundred and first year, in the first month, the first day of the month, the waters were dried from off the earth. And Noah removed the covering of the ark and looked, and behold, the face of the ground was dry. 14In the second month, on the twenty-seventh day of the month, the earth had dried out. 15Then God said to Noah, 16“Go out from the ark, you and your wife, and your sons and your sons’ wives with you. 17Bring out with you every living thing that is with you of all flesh—birds and animals and every creeping thing that creeps on the earth—that they may swarm on the earth, and be fruitful and multiply on the earth.” 18So Noah went out, and his sons and his wife and his sons’ wives with him. 19Every beast, every creeping thing, and every bird, everything that moves on the earth, went out by families from the ark.
-20Then Noah built an altar to the Lord and took some of every clean animal and some of every clean bird and offered burnt offerings on the altar. 21And when the Lord smelled the pleasing aroma, the Lord said in his heart, “I will never again curse the ground because of man, for the intention of man’s heart is evil from his youth. Neither will I ever again strike down every living creature as I have done. 22While the earth remains, seedtime and harvest, cold and heat, summer and winter, day and night, shall not cease.”`
+theirs = `1And God blessed Noah and his sons and said to them, “Be fruitful and multiply and fill the earth. 2The fear of you and the dread of you shall be upon every beast of the earth and upon every bird of the heavens, upon everything that creeps on the ground and all the fish of the sea. Into your hand they are delivered. 3Every moving thing that lives shall be food for you. And as I gave you the green plants, I give you everything. 4But you shall not eat flesh with its life, that is, its blood. 5And for your lifeblood I will require a reckoning: from every beast I will require it and from man. From his fellow man I will require a reckoning for the life of man.
+6 “Whoever sheds the blood of man,
+by man shall his blood be shed,
+for God made man in his own image.
+7And you, be fruitful and multiply, increase greatly on the earth and multiply in it.”
+8Then God said to Noah and to his sons with him, 9“Behold, I establish my covenant with you and your offspring after you, 10and with every living creature that is with you, the birds, the livestock, and every beast of the earth with you, as many as came out of the ark; it is for every beast of the earth. 11I establish my covenant with you, that never again shall all flesh be cut off by the waters of the flood, and never again shall there be a flood to destroy the earth.” 12And God said, “This is the sign of the covenant that I make between me and you and every living creature that is with you, for all future generations: 13I have set my bow in the cloud, and it shall be a sign of the covenant between me and the earth. 14When I bring clouds over the earth and the bow is seen in the clouds, 15I will remember my covenant that is between me and you and every living creature of all flesh. And the waters shall never again become a flood to destroy all flesh. 16When the bow is in the clouds, I will see it and remember the everlasting covenant between God and every living creature of all flesh that is on the earth.” 17God said to Noah, “This is the sign of the covenant that I have established between me and all flesh that is on the earth.”
+18The sons of Noah who went forth from the ark were Shem, Ham, and Japheth. (Ham was the father of Canaan.) 19These three were the sons of Noah, and from these the people of the whole earth were dispersed.
+20Noah began to be a man of the soil, and he planted a vineyard. 21He drank of the wine and became drunk and lay uncovered in his tent. 22And Ham, the father of Canaan, saw the nakedness of his father and told his two brothers outside. 23Then Shem and Japheth took a garment, laid it on both their shoulders, and walked backward and covered the nakedness of their father. Their faces were turned backward, and they did not see their father’s nakedness. 24When Noah awoke from his wine and knew what his youngest son had done to him, 25he said,
+“Cursed be Canaan;
+a servant of servants shall he be to his brothers.”
+26He also said,
+“Blessed be the Lord, the God of Shem;
+and let Canaan be his servant.
+27May God enlarge Japheth,
+and let him dwell in the tents of Shem,
+and let Canaan be his servant.”
+28After the flood Noah lived 350 years. 29All the days of Noah were 950 years, and he died.`
 theirs = theirs.split("\n").join(" ")
 theirs = theirs.split("“").join('"')
 theirs = theirs.split("”").join('"')
