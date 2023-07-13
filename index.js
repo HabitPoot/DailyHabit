dca46a3306ca73aaed4b8368d3859f0dc65384ab
HabitPoot <137962722+HabitPoot@users.noreply.github.com>
2023-07-13 03:54:03 -0700

    trying again

--- index.js
+++ index.js
@@ -921,15 +921,7 @@ right.addEventListener("click", function () {
 up.addEventListener("click", function(){
     window.scrollTo({top:0, behavior:'smooth'})
 })
-addEventListener("scroll", (event) => {
-    if(window.scrollY === 0){
-        up.classList.add("hidden")
-    } else {
-
-        up.classList.remove("hidden")
-    }
-});
-addEventListener("touchmove", (event) => {
+window.addEventListener("scroll", (event) => {
     if(window.scrollY === 0){
         up.classList.add("hidden")
     } else {
