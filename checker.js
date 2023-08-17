789e0ce7bce8343b0642aac826619a5a06bc7d16
HabitPoot <137962722+HabitPoot@users.noreply.github.com>
2023-08-16 18:22:00 -0700

    updates

--- checker.js
+++ checker.js
@@ -1,39 +1,55 @@
 mine = [
-    "1| Then Joseph could not control himself before all those who stood by him. He cried, \"Make everyone go out from me.\" So no one stayed with him when Joseph made himself known to his brothers.",
-    "2| And he wept aloud, so that the Egyptians heard it, and the household of Pharaoh heard it.",
-    "3| And Joseph said to his brothers, \"I am Joseph! Is my father still alive?\" But his brothers could not answer him, for they were dismayed at his presence.",
-    "4| So Joseph said to his brothers, \"Come near to me, please.\" And they came near. And he said, \"I am your brother, Joseph, whom you sold into Egypt.",
-    "5| And now do not be distressed or angry with yourselves because you sold me here, for God sent me before you to preserve life.",
-    "6| For the famine has been in the land these two years, and there are yet five years in which there will be neither plowing nor harvest.",
-    "7| And God sent me before you to preserve for you a remnant on earth, and to keep alive for you many survivors.",
-    "8| So it was not you who sent me here, but God. He has made me a father to Pharaoh, and lord of all his house and ruler over all the land of Egypt.",
-    "9| Hurry and go up to my father and say to him, 'Thus says your son Joseph, God has made me lord of all Egypt. Come down to me; do not tarry.",
-    "10| You shall dwell in the land of Goshen, and you shall be near me, you and your children and your children's children, and your flocks, your herds, and all that you have.",
-    "11| There I will provide for you, for there are yet five years of famine to come, so that you and your household, and all that you have, do not come to poverty.'",
-    "12| And now your eyes see, and the eyes of my brother Benjamin see, that it is my mouth that speaks to you.",
-    "13| You must tell my father of all my honor in Egypt, and of all that you have seen. Hurry and bring my father down here.\"",
-    "14| Then he fell upon his brother Benjamin's neck and wept, and Benjamin wept upon his neck.",
-    "15| And he kissed all his brothers and wept upon them. After that his brothers talked with him.",
-    "16| When the report was heard in Pharaoh's house, \"Joseph's brothers have come,\" it pleased Pharaoh and his servants.",
-    "17| And Pharaoh said to Joseph, \"Say to your brothers, 'Do this: load your beasts and go back to the land of Canaan,",
-    "18| and take your father and your households, and come to me, and I will give you the best of the land of Egypt, and you shall eat the fat of the land.'",
-    "19| And you, Joseph, are commanded to say, 'Do this: take wagons from the land of Egypt for your little ones and for your wives, and bring your father, and come.",
-    "20| Have no concern for your goods, for the best of all the land of Egypt is yours.'\"",
-    "21| The sons of Israel did so: and Joseph gave them wagons, according to the command of Pharaoh, and gave them provisions for the journey.",
-    "22| To each and all of them he gave a change of clothes, but to Benjamin he gave three hundred shekels of silver and five changes of clothes.",
-    "23| To his father he sent as follows: ten donkeys loaded with the good things of Egypt, and ten female donkeys loaded with grain, bread, and provision for his father on the journey.",
-    "24| Then he sent his brothers away, and as they departed, he said to them, \"Do not quarrel on the way.\"",
-    "25| So they went up out of Egypt and came to the land of Canaan to their father Jacob.",
-    "26| And they told him, \"Joseph is still alive, and he is ruler over all the land of Egypt.\" And his heart became numb, for he did not believe them.",
-    "27| But when they told him all the words of Joseph, which he had said to them, and when he saw the wagons that Joseph had sent to carry him, the spirit of their father Jacob revived.",
-    "28| And Israel said, \"It is enough; Joseph my son is still alive. I will go and see him before I die.\"",
+    "1| 이스라엘이 모든 소유를 이끌고 발행하여 브엘세바에 이르러 그 아비 이삭의 하나님께 희생을 드리니",
+    "2| 밤에 하나님이 이상 중에 이스라엘에게 나타나시고 불러 가라사대 야곱아 야곱아 하시는지라 야곱이 가로되 내가 여기 있나이다 하매",
+    "3| 하나님이 가라사대 나는 하나님이라 네 아비의 하나님이니 애굽으로 내려가기를 두려워 말라 내가 거기서 너로 큰 민족을 이루게 하리라",
+    "4| 내가 너와 함께 애굽으로 내려가겠고 정녕 너를 인도하여 다시 올라올 것이며 요셉이 그 손으로 네 눈을 감기리라 하셨더라",
+    "5| 야곱이 브엘세바에서 발행할쌔 이스라엘의 아들들이 바로의 태우려고 보낸 수레에 자기들의 아비 야곱과 자기들의 처자들을 태웠고",
+    "6| 그 생축과 가나안 땅에서 얻은 재물을 이끌었으며 야곱과 그 자손들이 다 함께 애굽으로 갔더라",
+    "7| 이와 같이 야곱이 그 아들들과 손자들과 딸들과 손녀들 곧 그 모든 자손을 데리고 애굽으로 갔더라",
+    "8| 애굽으로 내려간 이스라엘 가족의 이름이 이러하니 야곱과 그 아들들 곧 야곱의 맏아들 르우벤과",
+    "9| 르우벤의 아들 하녹과 발루와 헤스론과 갈미요",
+    "10| 시므온의 아들 곧 여무엘과 야민과 오핫과 야긴과 스할과 가나안 여인의 소생 사울이요",
+    "11| 레위의 아들 곧 게르손과 그핫과 므라리요",
+    "12| 유다의 아들 곧 엘과 오난과 셀라와 베레스와 세라니 엘과 오난은 가나안 땅에서 죽었고 또 베레스의 아들 곧 헤스론과 하물이요",
+    "13| 잇사갈의 아들 곧 돌라와 부와와 욥과 시므론이요",
+    "14| 스불론의 아들 곧 세렛과 엘론과 얄르엘이니",
+    "15| 이들은 레아가 밧단아람에서 야곱에게 낳은 자손들이라 그 딸 디나를 합하여 남자와 여자가 삼십 삼명이며",
+    "16| 갓의 아들 곧 시뵨과 학기와 수니와 에스본과 에리와 아로디와 아렐리요",
+    "17| 아셀의 아들 곧 임나와 이스와와 이스위와 브리아와 그들의 누이 세라며 또 브리아의 아들 곧 헤벨과 말기엘이니",
+    "18| 이들은 라반이 그 딸 레아에게 준 실바가 야곱에게 낳은 자손들이라 합 십 륙명이요",
+    "19| 야곱의 아내 라헬의 아들 곧 요셉과 베냐민이요",
+    "20| 애굽 땅에서 온 제사장 보디베라의 딸 아스낫이 요셉에게 낳은 므낫세와 에브라임이요",
+    "21| 베냐민의 아들 곧 벨라와 베겔과 아스벨과 게라와 나아만과 에히와 로스와 뭅빔과 훕빔과 아릇이니",
+    "22| 이들은 라헬이 야곱에게 낳은 자손이라 합 십 사명이요",
+    "23| 단의 아들 후심이요",
+    "24| 납달리의 아들 곧 야스엘과 구니와 예셀과 실렘이라",
+    "25| 이들은 라반이 그 딸 라헬에게 준 빌하가 야곱에게 낳은 자손이니 합이 칠명이라",
+    "26| 야곱과 함께 애굽에 이른 자는 야곱의 자부 외에 육십 륙명이니 이는 다 야곱의 몸에서 나온 자며",
+    "27| 애굽에서 요셉에게 낳은 아들이 두명이니 야곱의 집 사람으로 애굽에 이른 자의 도합이 칠십명이었더라",
+    "28| 야곱이 유다를 요셉에게 미리 보내어 자기를 고센으로 인도하게 하고 다 고센 땅에 이르니",
+    "29| 요셉이 수레를 갖추고 고센으로 올라가서 아비 이스라엘을 맞으며 그에게 보이고 그 목을 어긋맞겨 안고 얼마 동안 울매",
+    "30| 이스라엘이 요셉에게 이르되 네가 지금까지 살아 있고 내가 네 얼굴을 보았으니 지금 죽어도 가하도다",
+    "31| 요셉이 그 형들과 아비의 권속에게 이르되 내가 올라가서 바로에게 고하여 이르기를 가나안 땅에 있던 내 형들과 내 아비의 권속이 내게로 왔는데",
+    "32| 그들은 목자라 목축으로 업을 삼으므로 그 양과 소와 모든 소유를 이끌고 왔나이다 하리니",
+    "33| 바로가 당신들을 불러서 너희의 업이 무엇이냐 묻거든",
+    "34| 당신들은 고하기를 주의 종들은 어렸을 때부터 지금까지 목축하는 자이온데 우리와 우리 선조가 다 그러하니이다 하소서 애굽 사람은 다 목축을 가증히 여기나니 당신들이 고센 땅에 거하게 되리이다",
 ]
 let encoded = ` “ ” `
-theirs = `1Then Joseph could not control himself before all those who stood by him. He cried, “Make everyone go out from me.” So no one stayed with him when Joseph made himself known to his brothers. 2And he wept aloud, so that the Egyptians heard it, and the household of Pharaoh heard it. 3And Joseph said to his brothers, “I am Joseph! Is my father still alive?” But his brothers could not answer him, for they were dismayed at his presence.
-4So Joseph said to his brothers, “Come near to me, please.” And they came near. And he said, “I am your brother, Joseph, whom you sold into Egypt. 5And now do not be distressed or angry with yourselves because you sold me here, for God sent me before you to preserve life. 6For the famine has been in the land these two years, and there are yet five years in which there will be neither plowing nor harvest. 7And God sent me before you to preserve for you a remnant on earth, and to keep alive for you many survivors. 8So it was not you who sent me here, but God. He has made me a father to Pharaoh, and lord of all his house and ruler over all the land of Egypt. 9Hurry and go up to my father and say to him, ‘Thus says your son Joseph, God has made me lord of all Egypt. Come down to me; do not tarry. 10You shall dwell in the land of Goshen, and you shall be near me, you and your children and your children’s children, and your flocks, your herds, and all that you have. 11There I will provide for you, for there are yet five years of famine to come, so that you and your household, and all that you have, do not come to poverty.’ 12And now your eyes see, and the eyes of my brother Benjamin see, that it is my mouth that speaks to you. 13You must tell my father of all my honor in Egypt, and of all that you have seen. Hurry and bring my father down here.” 14Then he fell upon his brother Benjamin’s neck and wept, and Benjamin wept upon his neck. 15And he kissed all his brothers and wept upon them. After that his brothers talked with him.
-16When the report was heard in Pharaoh’s house, “Joseph’s brothers have come,” it pleased Pharaoh and his servants. 17And Pharaoh said to Joseph, “Say to your brothers, ‘Do this: load your beasts and go back to the land of Canaan, 18and take your father and your households, and come to me, and I will give you the best of the land of Egypt, and you shall eat the fat of the land.’ 19And you, Joseph, are commanded to say, ‘Do this: take wagons from the land of Egypt for your little ones and for your wives, and bring your father, and come. 20Have no concern for your goods, for the best of all the land of Egypt is yours.’”
-21The sons of Israel did so: and Joseph gave them wagons, according to the command of Pharaoh, and gave them provisions for the journey. 22To each and all of them he gave a change of clothes, but to Benjamin he gave three hundred shekels of silver and five changes of clothes. 23To his father he sent as follows: ten donkeys loaded with the good things of Egypt, and ten female donkeys loaded with grain, bread, and provision for his father on the journey. 24Then he sent his brothers away, and as they departed, he said to them, “Do not quarrel on the way.”
-25So they went up out of Egypt and came to the land of Canaan to their father Jacob. 26And they told him, “Joseph is still alive, and he is ruler over all the land of Egypt.” And his heart became numb, for he did not believe them. 27But when they told him all the words of Joseph, which he had said to them, and when he saw the wagons that Joseph had sent to carry him, the spirit of their father Jacob revived. 28And Israel said, “It is enough; Joseph my son is still alive. I will go and see him before I die.”`
+theirs = `1이스라엘이 모든 소유를 이끌고 발행하여 브엘세바에 이르러 그 아비 이삭의 하나님께 희생을 드리니 2밤에 하나님이 이상 중에 이스라엘에게 나타나시고 불러 가라사대 야곱아 야곱아 하시는지라 야곱이 가로되 내가 여기 있나이다 하매 3하나님이 가라사대 나는 하나님이라 네 아비의 하나님이니 애굽으로 내려가기를 두려워 말라 내가 거기서 너로 큰 민족을 이루게 하리라 4내가 너와 함께 애굽으로 내려가겠고 정녕 너를 인도하여 다시 올라올 것이며 요셉이 그 손으로 네 눈을 감기리라 하셨더라 5야곱이 브엘세바에서 발행할쌔 이스라엘의 아들들이 바로의 태우려고 보낸 수레에 자기들의 아비 야곱과 자기들의 처자들을 태웠고
+6그 생축과 가나안 땅에서 얻은 재물을 이끌었으며 야곱과 그 자손들이 다 함께 애굽으로 갔더라 7이와 같이 야곱이 그 아들들과 손자들과 딸들과 손녀들 곧 그 모든 자손을 데리고 애굽으로 갔더라 8애굽으로 내려간 이스라엘 가족의 이름이 이러하니 야곱과 그 아들들 곧 야곱의 맏아들 르우벤과
+9르우벤의 아들 하녹과 발루와 헤스론과 갈미요 10시므온의 아들 곧 여무엘과 야민과 오핫과 야긴과 스할과 가나안 여인의 소생 사울이요
+11레위의 아들 곧 게르손과 그핫과 므라리요
+12유다의 아들 곧 엘과 오난과 셀라와 베레스와 세라니 엘과 오난은 가나안 땅에서 죽었고 또 베레스의 아들 곧 헤스론과 하물이요
+13잇사갈의 아들 곧 돌라와 부와와 욥과 시므론이요
+14스불론의 아들 곧 세렛과 엘론과 얄르엘이니
+15이들은 레아가 밧단아람에서 야곱에게 낳은 자손들이라 그 딸 디나를 합하여 남자와 여자가 삼십 삼명이며 16갓의 아들 곧 시뵨과 학기와 수니와 에스본과 에리와 아로디와 아렐리요
+17아셀의 아들 곧 임나와 이스와와 이스위와 브리아와 그들의 누이 세라며 또 브리아의 아들 곧 헤벨과 말기엘이니
+18이들은 라반이 그 딸 레아에게 준 실바가 야곱에게 낳은 자손들이라 합 십 륙명이요 19야곱의 아내 라헬의 아들 곧 요셉과 베냐민이요 20애굽 땅에서 온 제사장 보디베라의 딸 아스낫이 요셉에게 낳은 므낫세와 에브라임이요
+21베냐민의 아들 곧 벨라와 베겔과 아스벨과 게라와 나아만과 에히와 로스와 뭅빔과 훕빔과 아릇이니
+22이들은 라헬이 야곱에게 낳은 자손이라 합 십 사명이요 23단의 아들 후심이요
+24납달리의 아들 곧 야스엘과 구니와 예셀과 실렘이라
+25이들은 라반이 그 딸 라헬에게 준 빌하가 야곱에게 낳은 자손이니 합이 칠명이라 26야곱과 함께 애굽에 이른 자는 야곱의 자부 외에 육십 륙명이니 이는 다 야곱의 몸에서 나온 자며 27애굽에서 요셉에게 낳은 아들이 두명이니 야곱의 집 사람으로 애굽에 이른 자의 도합이 칠십명이었더라 28야곱이 유다를 요셉에게 미리 보내어 자기를 고센으로 인도하게 하고 다 고센 땅에 이르니
+29요셉이 수레를 갖추고 고센으로 올라가서 아비 이스라엘을 맞으며 그에게 보이고 그 목을 어긋맞겨 안고 얼마 동안 울매 30이스라엘이 요셉에게 이르되 네가 지금까지 살아 있고 내가 네 얼굴을 보았으니 지금 죽어도 가하도다 31요셉이 그 형들과 아비의 권속에게 이르되 내가 올라가서 바로에게 고하여 이르기를 가나안 땅에 있던 내 형들과 내 아비의 권속이 내게로 왔는데 32그들은 목자라 목축으로 업을 삼으므로 그 양과 소와 모든 소유를 이끌고 왔나이다 하리니 33바로가 당신들을 불러서 너희의 업이 무엇이냐 묻거든 34당신들은 고하기를 주의 종들은 어렸을 때부터 지금까지 목축하는 자이온데 우리와 우리 선조가 다 그러하니이다 하소서 애굽 사람은 다 목축을 가증히 여기나니 당신들이 고센 땅에 거하게 되리이다`
 theirs = theirs.split("\n").join(" ")
 theirs = theirs.split("“").join('"')
 theirs = theirs.split("”").join('"')
