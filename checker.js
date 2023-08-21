1956056d20805f30ad74c3c6ee4b8c94f3db3624
HabitPoot <137962722+HabitPoot@users.noreply.github.com>
2023-08-21 16:40:12 -0700

    updates

--- checker.js
+++ checker.js
@@ -1,36 +1,31 @@
 mine = [
-    "1| Then Joseph fell on his father's face and wept over him and kissed him.",
-    "2| And Joseph commanded his servants the physicians to embalm his father. So the physicians embalmed Israel.",
-    "3| Forty days were required for it, for that is how many are required for embalming. And the Egyptians wept for him seventy days.",
-    "4| And when the days of weeping for him were past, Joseph spoke to the household of Pharaoh, saying, \"If now I have found favor in your eyes, please speak in the ears of Pharaoh, saying,",
-    "5| 'My father made me swear, saying, \"I am about to die: in my tomb that I hewed out for myself in the land of Canaan, there shall you bury me.\" Now therefore, let me please go up and bury my father. Then I will return.'\"",
-    "6| And Pharaoh answered, \"Go up, and bury your father, as he made you swear.\"",
-    "7| So Joseph went up to bury his father. With him went up all the servants of Pharaoh, the elders of his household, and all the elders of the land of Egypt,",
-    "8| as well as all the household of Joseph, his brothers, and his father's household. Only their children, their flocks, and their herds were left in the land of Goshen.",
-    "9| And there went up with him both chariots and horsemen. It was a very great company.",
-    "10| When they came to the threshing floor of Atad, which is beyond the Jordan, they lamented there with a very great and grievous lamentation, and he made a mourning for his father seven days.",
-    "11| When the inhabitants of the land, the Canaanites, saw the mourning on the threshing floor of Atad, they said, \"This is a grievous mourning by the Egyptians.\" Therefore the place was named Abel-mizraim; it is beyond the Jordan.",
-    "12| Thus his sons did for him as he had commanded them,",
-    "13| for his sons carried him to the land of Canaan and buried him in the cave of the field at Machpelah, to the east of Mamre, which Abraham bought with the field from Ephron the Hittite to possess as a burying place.",
-    "14| After he had buried his father, Joseph returned to Egypt with his brothers and all who had gone up with him to bury his father.",
-    "15| When Joseph's brothers saw that their father was dead, they said, \"It may be that Joseph will hate us and pay us back for all the evil that we did to him.\"",
-    "16| So they sent a message to Joseph, saying, \"Your father gave this command before he died:",
-    "17| 'Say to Joseph, \"Please forgive the transgression of your brothers and their sin, because they did evil to you.\"' And now, please forgive the transgression of the servants of the God of your father.\" Joseph wept when they spoke to him.",
-    "18| His brothers also came and fell down before him and said, \"Behold, we are your servants.\"",
-    "19| But Joseph said to them, \"Do not fear, for am I in the place of God?",
-    "20| As for you, you meant evil against me, but God meant it for good, to bring it about that many people should be kept alive, as they are today.",
-    "21| So do not fear; I will provide for you and your little ones.\" Thus he comforted them and spoke kindly to them.",
-    "22| So Joseph remained in Egypt, he and his father's house. Joseph lived 110 years.",
-    "23| And Joseph saw Ephraim's children of the third generation. The children also of Machir the son of Manasseh were counted as Joseph's own.",
-    "24| And Joseph said to his brothers, \"I am about to die, but God will visit you and bring you up out of this land to the land that he swore to Abraham, to Isaac, and to Jacob.\"",
-    "25| Then Joseph made the sons of Israel swear, saying, \"God will surely visit you, and you shall carry up my bones from here.\"",
-    "26| So Joseph died, being 110 years old. They embalmed him, and he was put in a coffin in Egypt.",
+    "1| These are the names of the sons of Israel who came to Egypt with Jacob, each with his household:",
+    "2| Reuben, Simeon, Levi, and Judah,",
+    "3| Issachar, Zebulun, and Benjamin,",
+    "4| Dan and Naphtali, Gad and Asher.",
+    "5| All the descendants of Jacob were seventy persons; Joseph was already in Egypt.",
+    "6| Then Joseph died, and all his brothers and all that generation.",
+    "7| But the people of Israel were fruitful and increased greatly; they multiplied and grew exceedingly strong, so that the land was filled with them.",
+    "8| Now there arose a new king over Egypt, who did not know Joseph.",
+    "9| And he said to his people, \"Behold, the people of Israel are too many and too mighty for us.",
+    "10| Come, let us deal shrewdly with them, lest they multiply, and, if war breaks out, they join our enemies and fight against us and escape from the land.\"",
+    "11| Therefore they set taskmasters over them to afflict them with heavy burdens. They built for Pharaoh store cities, Pithom and Raamses.",
+    "12| But the more they were oppressed, the more they multiplied and the more they spread abroad. And the Egyptians were in dread of the people of Israel.",
+    "13| So they ruthlessly made the people of Israel work as slaves",
+    "14| and made their lives bitter with hard service, in mortar and brick, and in all kinds of work in the field. In all their work they ruthlessly made them work as slaves.",
+    "15| Then the king of Egypt said to the Hebrew midwives, one of whom was named Shiphrah and the other Puah,",
+    "16| \"When you serve as midwife to the Hebrew women and see them on the birthstool, if it is a son, you shall kill him, but if it is a daughter, she shall live.\"",
+    "17| But the midwives feared God and did not do as the king of Egypt commanded them, but let the male children live.",
+    "18| So the king of Egypt called the midwives and said to them, \"Why have you done this, and let the male children live?\"",
+    "19| The midwives said to Pharaoh, \"Because the Hebrew women are not like the Egyptian women, for they are vigorous and give birth before the midwife comes to them.\"",
+    "20| So God dealt well with the midwives. And the people multiplied and grew very strong.",
+    "21| And because the midwives feared God, he gave them families.",
+    "22| Then Pharaoh commanded all his people, \"Every son that is born to the Hebrews you shall cast into the Nile, but you shall let every daughter live.\"",
 ]
 let encoded = ` “ ” `
-theirs = `1Then Joseph fell on his father’s face and wept over him and kissed him. 2And Joseph commanded his servants the physicians to embalm his father. So the physicians embalmed Israel. 3Forty days were required for it, for that is how many are required for embalming. And the Egyptians wept for him seventy days.
-4And when the days of weeping for him were past, Joseph spoke to the household of Pharaoh, saying, “If now I have found favor in your eyes, please speak in the ears of Pharaoh, saying, 5‘My father made me swear, saying, “I am about to die: in my tomb that I hewed out for myself in the land of Canaan, there shall you bury me.” Now therefore, let me please go up and bury my father. Then I will return.’” 6And Pharaoh answered, “Go up, and bury your father, as he made you swear.” 7So Joseph went up to bury his father. With him went up all the servants of Pharaoh, the elders of his household, and all the elders of the land of Egypt, 8as well as all the household of Joseph, his brothers, and his father’s household. Only their children, their flocks, and their herds were left in the land of Goshen. 9And there went up with him both chariots and horsemen. It was a very great company. 10When they came to the threshing floor of Atad, which is beyond the Jordan, they lamented there with a very great and grievous lamentation, and he made a mourning for his father seven days. 11When the inhabitants of the land, the Canaanites, saw the mourning on the threshing floor of Atad, they said, “This is a grievous mourning by the Egyptians.” Therefore the place was named Abel-mizraim; it is beyond the Jordan. 12Thus his sons did for him as he had commanded them, 13for his sons carried him to the land of Canaan and buried him in the cave of the field at Machpelah, to the east of Mamre, which Abraham bought with the field from Ephron the Hittite to possess as a burying place. 14After he had buried his father, Joseph returned to Egypt with his brothers and all who had gone up with him to bury his father.
-15When Joseph’s brothers saw that their father was dead, they said, “It may be that Joseph will hate us and pay us back for all the evil that we did to him.” 16So they sent a message to Joseph, saying, “Your father gave this command before he died: 17‘Say to Joseph, “Please forgive the transgression of your brothers and their sin, because they did evil to you.”’ And now, please forgive the transgression of the servants of the God of your father.” Joseph wept when they spoke to him. 18His brothers also came and fell down before him and said, “Behold, we are your servants.” 19But Joseph said to them, “Do not fear, for am I in the place of God? 20As for you, you meant evil against me, but God meant it for good, to bring it about that many people should be kept alive, as they are today. 21So do not fear; I will provide for you and your little ones.” Thus he comforted them and spoke kindly to them.
-22So Joseph remained in Egypt, he and his father’s house. Joseph lived 110 years. 23And Joseph saw Ephraim’s children of the third generation. The children also of Machir the son of Manasseh were counted as Joseph’s own. 24And Joseph said to his brothers, “I am about to die, but God will visit you and bring you up out of this land to the land that he swore to Abraham, to Isaac, and to Jacob.” 25Then Joseph made the sons of Israel swear, saying, “God will surely visit you, and you shall carry up my bones from here.” 26So Joseph died, being 110 years old. They embalmed him, and he was put in a coffin in Egypt.`
+theirs = `1These are the names of the sons of Israel who came to Egypt with Jacob, each with his household: 2Reuben, Simeon, Levi, and Judah, 3Issachar, Zebulun, and Benjamin, 4Dan and Naphtali, Gad and Asher. 5All the descendants of Jacob were seventy persons; Joseph was already in Egypt. 6Then Joseph died, and all his brothers and all that generation. 7But the people of Israel were fruitful and increased greatly; they multiplied and grew exceedingly strong, so that the land was filled with them.
+8Now there arose a new king over Egypt, who did not know Joseph. 9And he said to his people, “Behold, the people of Israel are too many and too mighty for us. 10Come, let us deal shrewdly with them, lest they multiply, and, if war breaks out, they join our enemies and fight against us and escape from the land.” 11Therefore they set taskmasters over them to afflict them with heavy burdens. They built for Pharaoh store cities, Pithom and Raamses. 12But the more they were oppressed, the more they multiplied and the more they spread abroad. And the Egyptians were in dread of the people of Israel. 13So they ruthlessly made the people of Israel work as slaves 14and made their lives bitter with hard service, in mortar and brick, and in all kinds of work in the field. In all their work they ruthlessly made them work as slaves.
+15Then the king of Egypt said to the Hebrew midwives, one of whom was named Shiphrah and the other Puah, 16“When you serve as midwife to the Hebrew women and see them on the birthstool, if it is a son, you shall kill him, but if it is a daughter, she shall live.” 17But the midwives feared God and did not do as the king of Egypt commanded them, but let the male children live. 18So the king of Egypt called the midwives and said to them, “Why have you done this, and let the male children live?” 19The midwives said to Pharaoh, “Because the Hebrew women are not like the Egyptian women, for they are vigorous and give birth before the midwife comes to them.” 20So God dealt well with the midwives. And the people multiplied and grew very strong. 21And because the midwives feared God, he gave them families. 22Then Pharaoh commanded all his people, “Every son that is born to the Hebrews you shall cast into the Nile, but you shall let every daughter live.”`
 theirs = theirs.split("\n").join(" ")
 theirs = theirs.split("“").join('"')
 theirs = theirs.split("”").join('"')
