09979d25c85bd62653f973cbd9780200611e19b1
HabitPoot <137962722+HabitPoot@users.noreply.github.com>
2023-09-13 08:14:26 -0700

    updates

--- checker.js
+++ checker.js
@@ -1,53 +1,29 @@
 mine = [
-    "1| \"You shall not spread a false report. You shall not join hands with a wicked man to be a malicious witness.",
-    "2| You shall not fall in with the many to do evil, nor shall you bear witness in a lawsuit, siding with the many, so as to pervert justice,",
-    "3| nor shall you be partial to a poor man in his lawsuit.",
-    "4| \"If you meet your enemy's ox or his donkey going astray, you shall bring it back to him.",
-    "5| If you see the donkey of one who hates you lying down under its burden, you shall refrain from leaving him with it; you shall rescue it with him.",
-    "6| \"You shall not pervert the justice due to your poor in his lawsuit.",
-    "7| Keep far from a false charge, and do not kill the innocent and righteous, for I will not acquit the wicked.",
-    "8| And you shall take no bribe, for a bribe blinds the clear-sighted and subverts the cause of those who are in the right.",
-    "9| \"You shall not oppress a sojourner. You know the heart of a sojourner, for you were sojourners in the land of Egypt.",
-    "10| \"For six years you shall sow your land and gather in its yield,",
-    "11| but the seventh year you shall let it rest and lie fallow, that the poor of your people may eat; and what they leave the beasts of the field may eat. You shall do likewise with your vineyard, and with your olive orchard.",
-    "12| \"Six days you shall do your work, but on the seventh day you shall rest; that your ox and your donkey may have rest, and the son of your servant woman, and the alien, may be refreshed.",
-    "13| \"Pay attention to all that I have said to you, and make no mention of the names of other gods, nor let it be heard on your lips.",
-    "14| \"Three times in the year you shall keep a feast to me.",
-    "15| You shall keep the Feast of Unleavened Bread. As I commanded you, you shall eat unleavened bread for seven days at the appointed time in the month of Abib, for in it you came out of Egypt. None shall appear before me empty-handed.",
-    "16| You shall keep the Feast of Harvest, of the firstfruits of your labor, of what you sow in the field. You shall keep the Feast of Ingathering at the end of the year, when you gather in from the field the fruit of your labor.",
-    "17| Three times in the year shall all your males appear before the Lord God.",
-    "18| \"You shall not offer the blood of my sacrifice with anything leavened, or let the fat of my feast remain until the morning.",
-    "19| \"The best of the firstfruits of your ground you shall bring into the house of the Lord your God. \"You shall not boil a young goat in its mother's milk.",
-    "20| \"Behold, I send an angel before you to guard you on the way and to bring you to the place that I have prepared.",
-    "21| Pay careful attention to him and obey his voice; do not rebel against him, for he will not pardon your transgression, for my name is in him.",
-    "22| \"But if you carefully obey his voice and do all that I say, then I will be an enemy to your enemies and an adversary to your adversaries.",
-    "23| \"When my angel goes before you and brings you to the Amorites and the Hittites and the Perizzites and the Canaanites, the Hivites and the Jebusites, and I blot them out,",
-    "24| you shall not bow down to their gods nor serve them, nor do as they do, but you shall utterly overthrow them and break their pillars in pieces.",
-    "25| You shall serve the Lord your God, and he will bless your bread and your water, and I will take sickness away from among you.",
-    "26| None shall miscarry or be barren in your land; I will fulfill the number of your days.",
-    "27| I will send my terror before you and will throw into confusion all the people against whom you shall come, and I will make all your enemies turn their backs to you.",
-    "28| And I will send hornets before you, which shall drive out the Hivites, the Canaanites, and the Hittites from before you.",
-    "29| I will not drive them out from before you in one year, lest the land become desolate and the wild beasts multiply against you.",
-    "30| Little by little I will drive them out from before you, until you have increased and possess the land.",
-    "31| And I will set your border from the Red Sea to the Sea of the Philistines, and from the wilderness to the Euphrates, for I will give the inhabitants of the land into your hand, and you shall drive them out before you.",
-    "32| You shall make no covenant with them and their gods.",
-    "33| They shall not dwell in your land, lest they make you sin against me; for if you serve their gods, it will surely be a snare to you.\"",
+    "1| Then he said to Moses, \"Come up to the Lord, you and Aaron, Nadab, and Abihu, and seventy of the elders of Israel, and worship from afar.",
+    "2| Moses alone shall come near to the Lord, but the others shall not come near, and the people shall not come up with him.\"",
+    "3| Moses came and told the people all the words of the Lord and all the rules. And all the people answered with one voice and said, \"All the words that the Lord has spoken we will do.\"",
+    "4| And Moses wrote down all the words of the Lord. He rose early in the morning and built an altar at the foot of the mountain, and twelve pillars, according to the twelve tribes of Israel.",
+    "5| And he sent young men of the people of Israel, who offered burnt offerings and sacrificed peace offerings of oxen to the Lord.",
+    "6| And Moses took half of the blood and put it in basins, and half of the blood he threw against the altar.",
+    "7| Then he took the Book of the Covenant and read it in the hearing of the people. And they said, \"All that the Lord has spoken we will do, and we will be obedient.\"",
+    "8| And Moses took the blood and threw it on the people and said, \"Behold the blood of the covenant that the Lord has made with you in accordance with all these words.\"",
+    "9| Then Moses and Aaron, Nadab, and Abihu, and seventy of the elders of Israel went up,",
+    "10| and they saw the God of Israel. There was under his feet as it were a pavement of sapphire stone, like the very heaven for clearness.",
+    "11| And he did not lay his hand on the chief men of the people of Israel; they beheld God, and ate and drank.",
+    "12| The Lord said to Moses, \"Come up to me on the mountain and wait there, that I may give you the tablets of stone, with the law and the commandment, which I have written for their instruction.\"",
+    "13| So Moses rose with his assistant Joshua, and Moses went up into the mountain of God.",
+    "14| And he said to the elders, \"Wait here for us until we return to you. And behold, Aaron and Hur are with you. Whoever has a dispute, let him go to them.\"",
+    "15| Then Moses went up on the mountain, and the cloud covered the mountain.",
+    "16| The glory of the Lord dwelt on Mount Sinai, and the cloud covered it six days. And on the seventh day he called to Moses out of the midst of the cloud.",
+    "17| Now the appearance of the glory of the Lord was like a devouring fire on the top of the mountain in the sight of the people of Israel.",
+    "18| Moses entered the cloud and went up on the mountain. And Moses was on the mountain forty days and forty nights.",
 ]
 let encoded = ` “ ” `
-theirs = `1“You shall not spread a false report. You shall not join hands with a wicked man to be a malicious witness. 2You shall not fall in with the many to do evil, nor shall you bear witness in a lawsuit, siding with the many, so as to pervert justice, 3nor shall you be partial to a poor man in his lawsuit.
-4“If you meet your enemy’s ox or his donkey going astray, you shall bring it back to him. 5If you see the donkey of one who hates you lying down under its burden, you shall refrain from leaving him with it; you shall rescue it with him.
-6“You shall not pervert the justice due to your poor in his lawsuit. 7Keep far from a false charge, and do not kill the innocent and righteous, for I will not acquit the wicked. 8And you shall take no bribe, for a bribe blinds the clear-sighted and subverts the cause of those who are in the right.
-9“You shall not oppress a sojourner. You know the heart of a sojourner, for you were sojourners in the land of Egypt.
-10“For six years you shall sow your land and gather in its yield, 11but the seventh year you shall let it rest and lie fallow, that the poor of your people may eat; and what they leave the beasts of the field may eat. You shall do likewise with your vineyard, and with your olive orchard.
-12“Six days you shall do your work, but on the seventh day you shall rest; that your ox and your donkey may have rest, and the son of your servant woman, and the alien, may be refreshed.
-13“Pay attention to all that I have said to you, and make no mention of the names of other gods, nor let it be heard on your lips.
-14“Three times in the year you shall keep a feast to me. 15You shall keep the Feast of Unleavened Bread. As I commanded you, you shall eat unleavened bread for seven days at the appointed time in the month of Abib, for in it you came out of Egypt. None shall appear before me empty-handed. 16You shall keep the Feast of Harvest, of the firstfruits of your labor, of what you sow in the field. You shall keep the Feast of Ingathering at the end of the year, when you gather in from the field the fruit of your labor. 17Three times in the year shall all your males appear before the Lord God.
-18“You shall not offer the blood of my sacrifice with anything leavened, or let the fat of my feast remain until the morning.
-19“The best of the firstfruits of your ground you shall bring into the house of the Lord your God.
-“You shall not boil a young goat in its mother’s milk.
-20“Behold, I send an angel before you to guard you on the way and to bring you to the place that I have prepared. 21Pay careful attention to him and obey his voice; do not rebel against him, for he will not pardon your transgression, for my name is in him.
-22“But if you carefully obey his voice and do all that I say, then I will be an enemy to your enemies and an adversary to your adversaries.
-23“When my angel goes before you and brings you to the Amorites and the Hittites and the Perizzites and the Canaanites, the Hivites and the Jebusites, and I blot them out, 24you shall not bow down to their gods nor serve them, nor do as they do, but you shall utterly overthrow them and break their pillars in pieces. 25You shall serve the Lord your God, and he will bless your bread and your water, and I will take sickness away from among you. 26None shall miscarry or be barren in your land; I will fulfill the number of your days. 27I will send my terror before you and will throw into confusion all the people against whom you shall come, and I will make all your enemies turn their backs to you. 28And I will send hornets before you, which shall drive out the Hivites, the Canaanites, and the Hittites from before you. 29I will not drive them out from before you in one year, lest the land become desolate and the wild beasts multiply against you. 30Little by little I will drive them out from before you, until you have increased and possess the land. 31And I will set your border from the Red Sea to the Sea of the Philistines, and from the wilderness to the Euphrates, for I will give the inhabitants of the land into your hand, and you shall drive them out before you. 32You shall make no covenant with them and their gods. 33They shall not dwell in your land, lest they make you sin against me; for if you serve their gods, it will surely be a snare to you.”`
+theirs = `1Then he said to Moses, “Come up to the Lord, you and Aaron, Nadab, and Abihu, and seventy of the elders of Israel, and worship from afar. 2Moses alone shall come near to the Lord, but the others shall not come near, and the people shall not come up with him.”
+3Moses came and told the people all the words of the Lord and all the rules. And all the people answered with one voice and said, “All the words that the Lord has spoken we will do.” 4And Moses wrote down all the words of the Lord. He rose early in the morning and built an altar at the foot of the mountain, and twelve pillars, according to the twelve tribes of Israel. 5And he sent young men of the people of Israel, who offered burnt offerings and sacrificed peace offerings of oxen to the Lord. 6And Moses took half of the blood and put it in basins, and half of the blood he threw against the altar. 7Then he took the Book of the Covenant and read it in the hearing of the people. And they said, “All that the Lord has spoken we will do, and we will be obedient.” 8And Moses took the blood and threw it on the people and said, “Behold the blood of the covenant that the Lord has made with you in accordance with all these words.”
+9Then Moses and Aaron, Nadab, and Abihu, and seventy of the elders of Israel went up, 10and they saw the God of Israel. There was under his feet as it were a pavement of sapphire stone, like the very heaven for clearness. 11And he did not lay his hand on the chief men of the people of Israel; they beheld God, and ate and drank.
+12The Lord said to Moses, “Come up to me on the mountain and wait there, that I may give you the tablets of stone, with the law and the commandment, which I have written for their instruction.” 13So Moses rose with his assistant Joshua, and Moses went up into the mountain of God. 14And he said to the elders, “Wait here for us until we return to you. And behold, Aaron and Hur are with you. Whoever has a dispute, let him go to them.”
+15Then Moses went up on the mountain, and the cloud covered the mountain. 16The glory of the Lord dwelt on Mount Sinai, and the cloud covered it six days. And on the seventh day he called to Moses out of the midst of the cloud. 17Now the appearance of the glory of the Lord was like a devouring fire on the top of the mountain in the sight of the people of Israel. 18Moses entered the cloud and went up on the mountain. And Moses was on the mountain forty days and forty nights.`
 theirs = theirs.split("\n").join(" ")
 theirs = theirs.split("“").join('"')
 theirs = theirs.split("”").join('"')
